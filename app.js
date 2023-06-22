93668f78d83fa8bb2c9704b693c0813a382ad3c5
Bishal Chansi <chansibishal55@gmail.com>
2023-06-22 16:12:56 +0545

    Backend API

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require("express");
+const app = express();
+
+const connectDB =require("./db/connect")
+
+const products_routes = require("./routes/products")
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req,res) => {
+    res.send("Hi I am live sknlsblvblj131313");
+});
+
+//middleware or to set router
+app.use("/api/products",products_routes)
+
+const start = async() =>{
+   try{
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT , () =>{
+        console.log(`${PORT} Yes I am Connnected`);
+    })
+   }catch(error){
+    console.log(error);
+   }
+}
+start();
